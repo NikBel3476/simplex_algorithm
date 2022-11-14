fb7030affc142e94e863e75fa71aa3b471564fda
NikBel3476 <nikita.belkin3476@gmail.com>
2022-11-14 21:26:19 +0400

    feature: add examples 2 and 3

--- simplex_algorithm.pas
+++ simplex_algorithm.pas
@@ -15,11 +15,11 @@ var
   originFuncCoefficients: array of real;
   matrixTransformationCount: integer := 1;
   matrixExample1 := new real[5, 6] (
-    (3.0, 1.0,  -4.0,  2.0,  -5.0, 9.0),
-    (6.0, 0.0,  1.0,  -3.0,  4.0, -5.0),
-    (1.0, 0.0, 1.0, -1.0, 1.0, -1.0),
-    (0.0, 2.0, 6.0, -5.0, 1.0, 4.0),
-    (-10.0, -1.0, 2.0, 2.0, 0.0, -3.0)
+    (  3.0,  1.0, -4.0,  2.0, -5.0,  9.0),
+    (  6.0,  0.0,  1.0, -3.0,  4.0, -5.0),
+    (  1.0,  0.0,  1.0, -1.0,  1.0, -1.0),
+    (  0.0,  2.0,  6.0, -5.0,  1.0,  4.0),
+    (-10.0, -1.0,  2.0,  2.0,  0.0, -3.0)
   );
   
   extendedMatrixExample1: ExtendedMatrix := (
@@ -28,53 +28,57 @@ var
     columnX: new string[5] ('x6=', 'x7=', 'x8=', 'f=', 'g=');
   );
   
-//  matrixExample2 := new real[3, 4] (
-//    (1.0, 1.0, 2.0,  3.0),
-//    (1.0, 1.0, 1.0,  1.0),
-//    (2.0, 1.0, 0.0, -1.0)
-//  );
-//  
-//  extendedMatrixExample2: ExtendedMatrix := (
-//    baseMatrix: matrixExample2;
-//    rowX: new string[3] ('-x1', '-x2', '-x3');
-//    columnX: new string[3] ('0=', '0=', '0=');
-//  );
-//  
-//  matrixExample3 := new real[3, 4] (
-//    (5.0, 1.0, 3.0,  -4.0),
-//    (0.0, -1.0, 1.0,  1.0),
-//    (9.0, 2.0, 1.0, 1.0)
-//  );
-//  
-//  extendedMatrixExample3: ExtendedMatrix := (
-//    baseMatrix: matrixExample3;
-//    rowX: new string[3] ('-x1', '-x2', '-x3');
-//    columnX: new string[3] ('0=', '0=', '0=');
-//  );
+  matrixExample2 := new real[5, 5] (
+    ( 3.0,   1.0,  2.0,  3.0,   4.0),
+    ( 1.0,   6.0,  2.0,  4.0,   8.0),
+    ( 5.0,   3.0,  2.0,  1.0,   2.0),
+    ( 0.0,   3.0,  1.0,  5.0,   4.0),
+    (-9.0, -10.0, -6.0, -8.0, -14.0)
+  );
+  
+  extendedMatrixExample2: ExtendedMatrix := (
+    baseMatrix: matrixExample2;
+    rowX: new string[4] ('-x1', '-x2', '-x3', '-x4');
+    columnX: new string[5] ('x5=', 'x6=', 'x7=', 'f=', 'g=');
+  );
+  
+  matrixExample3 := new real[5, 4] (
+    ( 4.0, -3.0,   8.0,  1.0),
+    ( 2.0, -5.0,  -3.0, -1.0),
+    ( 3.0, 15.0,  10.0, -2.0),
+    ( 0.0, -2.0,   1.0,  3.0),
+    (-9.0, -7.0, -15.0,  2.0)
+  );
+  
+  extendedMatrixExample3: ExtendedMatrix := (
+    baseMatrix: matrixExample3;
+    rowX: new string[3] ('-x1', '-x2', '-x3');
+    columnX: new string[5] ('x4=', 'x5=', 'x6=', 'f=', 'g=');
+  );
 
 begin
 //  Console.OutputEncoding := System.Text.Encoding.GetEncoding(866);
   extendedResultMatrix := extendedMatrixExample1;
+  
   originFuncCoefficients := extendedResultMatrix.baseMatrix
     .Row(extendedResultMatrix.baseMatrix.GetLength(0) - 2).Skip(1).ToArray();
   
   Writeln('Исходная матрица');
   extendedResultMatrix.Print();
+  Writeln('------------------------------------------------------------------');
   
   // part 1
   while (true) do
   begin
     pivotColumnIndex := extendedResultMatrix.FindPivotColumnIndex();
-    Writeln('Индекс разрешающего столбца: ', pivotColumnIndex);
-    
     pivotRowIndex := extendedResultMatrix.FindPivotRowIndex(pivotColumnIndex);
-    Writeln('Индекс разрешающей строки: ', pivotRowIndex + 1);
-    
+    Writeln('Индекс разрешающего элемента: (', pivotRowIndex + 1, ', ', pivotColumnIndex, ')');
     Writeln('Разрешающий элемент: ', extendedResultMatrix.baseMatrix[pivotRowIndex, pivotColumnIndex]);
-    tempMatrix := GaussianElimination(extendedResultMatrix.baseMatrix, (pivotRowIndex, pivotColumnIndex));
     
+    tempMatrix := GaussianElimination(extendedResultMatrix.baseMatrix, (pivotRowIndex, pivotColumnIndex));
     extendedResultMatrix.baseMatrix := tempMatrix;
     extendedResultMatrix.SwapRowAndColumnVariables(pivotRowIndex, pivotColumnIndex - 1);
+    
     Writeln('Преобразование ', matrixTransformationCount);
     extendedResultMatrix.Print();
     
@@ -141,14 +145,14 @@ begin
       end;
       // поиск отрицательного элемента в столбце. end
       
-      Writeln('Индекс разрешающего столбца: ', pivotColumnIndex);
-      
       pivotRowIndex := extendedResultMatrix.FindPivotRowIndex(pivotColumnIndex);
-      Writeln('Индекс разрешающей строки: ', pivotRowIndex + 1);
+      Writeln('Индекс разрешающего элемента: (', pivotRowIndex + 1, ', ', pivotColumnIndex, ')');
+      Writeln('Разрешающий элемент: ', extendedResultMatrix.baseMatrix[pivotRowIndex, pivotColumnIndex]);
       
       tempMatrix := gaussianElimination(extendedResultMatrix.baseMatrix, (pivotRowIndex, pivotColumnIndex));
       extendedResultMatrix.baseMatrix := tempMatrix;
       extendedResultMatrix.SwapRowAndColumnVariables(pivotRowIndex, pivotColumnIndex - 1);
+      
       Writeln('Преобразование ', matrixTransformationCount);
       extendedResultMatrix.Print();
       
@@ -157,7 +161,7 @@ begin
     end
     else
     begin
-      Writeln('Решение:');
+      Writeln('Ответ:');
       
       // исключаем строки g, f и столбец уравнения, поэтому длина - 3
       var xValues := ArrFill(
@@ -175,18 +179,22 @@ begin
       end;
       
       // вывод ответа
-      Write('x = ');
+      Write('x = ( ');
       for var i := 0 to xValues.Length - 1 do
       begin
-        Write(xValues[i], '  ');
+        Write(xValues[i]:0:2, '  ');
       end;
-      Writeln();
+      Writeln(')');
       
-      var answer := 0.0;
-      for var i := 0 to extendedResultMatrix.baseMatrix.ColCount() - 2 do
-        answer += -originFuncCoefficients[i] * xValues[i];
-      
-      Writeln('F(x) = ', answer:0:3);
+      if (xValues.TakeLast(extendedResultMatrix.baseMatrix.GetLength(0) - 2).All(x -> x = 0)) then
+      begin
+        var answer := 0.0;
+        for var i := 0 to extendedResultMatrix.baseMatrix.ColCount() - 2 do
+          answer += -originFuncCoefficients[i] * xValues[i];
+        Writeln('F(x) = ', answer:0:3)
+      end
+      else        
+        Writeln('Исходная задача не имеет опорного плана');
       
       exit;
     end;
